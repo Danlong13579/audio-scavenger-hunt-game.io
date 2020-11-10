37333a949dfd93b845b2bccc50f78fafd66fb843
Daniel Long <danlong13579@gmail.com>
2020-11-10 11:56:58 -0500

    random button placement & distance calc

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ let gameStart = false
 let GameSpace = document.getElementsByTagName('body')[0]
 let darkModeSwitch = document.getElementById('dark-mode')
 let startButton = document.getElementById('start')
+let gameButton = document.createElement('button')
+
+// Hidden button class setup
+gameButton.className = "game-button"
+let buttonX = Math.floor(Math.random() * (document.body.clientWidth - 32))
+let buttonY = Math.floor(Math.random() * (document.body.clientHeight - 32))
+gameButton.style.top = buttonY + "px"
+gameButton.style.left = buttonX + "px"
+
 
 // Audio Setup
 let heartAudio = document.getElementById('game-audio')
@@ -16,6 +25,7 @@ startButton.addEventListener('click', function(e) {
     console.log("Game Started")
     // Remove game-root from the screen and setup
     document.getElementById('game-root').style = "display: none;"
+    document.body.appendChild(gameButton)
     NormalizeAudio()
 
     // Check Platform for devices
@@ -23,23 +33,25 @@ startButton.addEventListener('click', function(e) {
 
         // Logic for touchscreen devices
         document.body.addEventListener("touchstart", function(e) {  
-            mouseUpdate(e)
             heartAudio.play()     
-        })
+        } )
+        document.body.addEventListener('touchmove', function(e) {
+            MouseUpdate(e)
+        } )
         document.body.addEventListener("touchend", function(e) { 
             heartAudio.pause()   
-        })
+        } )
     } else {
 
         //Logic for the web
         document.body.addEventListener("mousemove", function(e) {
-            mouseUpdate(e)
-            heartAudio.play()
-        })
+            MouseUpdate(e)
+            heartAudio.play() 
+        } )
             
         document.body.addEventListener("mouseout", function(e) {
             heartAudio.pause()
-        })
+        } )
     }
 } )
 
@@ -52,17 +64,23 @@ darkModeSwitch.addEventListener('click', function(e){
         console.log("on")
         GameSpace.style = "background-color: #333333;"
     }
-
 } )
 
 // Functions
-function mouseUpdate(e){
-    let x = e.clientX
-    let y = e.clientY
-    let cords = `Cords X = ${x} and Y = ${y}`
-    console.log(cords)
+function MouseUpdate(e){
+
+    // calculate the distance between the mouse or finger to the button on screen
+    let deltaX = Math.abs(e.clientX - buttonX)
+    let deltaY = Math.abs(e.clientY - buttonY)
+    let distance = Math.floor(Math.sqrt( Math.pow(deltaX, 2) + Math.pow(deltaY, 2)))
+    console.log(distance)
+
+    // TODO calc distance % to button area including screen space
+    let dist = ""
+    console.log ("the distance in % is " + dist + "%")
+
 }
 
-function NormalizeAudio(vol = 0.2){
+function NormalizeAudio(vol = 0.4){
     heartAudio.volume = vol
 }
